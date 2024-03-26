3a59c2cf9bc79cef62d89dee1b5c8255f5111782
Gianluigi Forte <koalakoker@gmail.com>
2024-03-26 07:31:27 +0100

    Road implemented

--- main.js
+++ main.js
@@ -4,10 +4,13 @@ canvas.width = 400;
 var ctx = canvas.getContext("2d");
 var car = new Car(canvas.width / 2, window.innerHeight / 2, 40, 80);
 
+var road = new Road(canvas.width / 2, canvas.width - 20, 3);
+
 animate();
 
 function animate() {
   canvas.height = window.innerHeight;
+  road.draw(ctx);
   car.draw(ctx);
   requestAnimationFrame(animate);
 }
