0de8e43ee1af5f1d39bbff5ffa167b53b0adcdd2
Gianluigi Forte <koalakoker@gmail.com>
2024-03-28 19:49:30 +0100

    Working on inductor

---
+++ inductor.js
@@ -0,0 +1,86 @@
+class Inductor {
+  constructor(amp, xStart, xEnd, y0, wave, leg) {
+    this.amp = amp;
+    this.xStart = xStart;
+    this.xEnd = xEnd;
+    this.y0 = y0;
+    this.wave = wave;
+    this.leg = leg;
+    this.dTh = (this.wave * (2 * Math.PI)) / (this.xEnd - this.xStart);
+    this.off = 0;
+    this.step = 5;
+    this.speed = 0.0;
+    this.dir = 1;
+    this.on = false;
+    this.flux = new Vector("blue", this.speed, 0, { x: 300, y: 300 });
+  }
+
+  animate() {
+    this.speed += 0.01 * this.dir;
+    if (this.speed >= 1 || this.speed <= -1) {
+      this.dir = this.dir * -1;
+    }
+    this.flux.mag = this.speed;
+  }
+
+  draw(ctx) {
+    this.frame(ctx);
+    if (this.on) {
+      this.flux.draw(ctx);
+      this.electron(ctx);
+    }
+    this.animate();
+  }
+
+  electron(ctx) {
+    ctx.fillStyle = "red";
+    for (let x = xStart - leg + this.off; x < xEnd + leg; x += this.step) {
+      let y = parseInt(this.calc(x));
+      ctx.beginPath();
+      ctx.arc(x, y, 5, 0, Math.PI * 2, true); // Outer circle
+      ctx.fill();
+    }
+    this.off += this.speed;
+    if (this.off > this.step) {
+      this.off = 0;
+    }
+  }
+
+  frame(ctx) {
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 4;
+    ctx.beginPath();
+
+    let y = this.y0;
+    let th = 0;
+    let dTh = (this.wave * (2 * Math.PI)) / (this.xEnd - this.xStart);
+    ctx.moveTo(this.xStart - this.leg, y);
+    ctx.lineTo(this.xStart, this.y0);
+    for (let x = this.xStart; x <= this.xEnd; x += 1) {
+      y = this.y0 + this.amp * Math.sin(th);
+      th += dTh;
+      ctx.lineTo(x, y);
+    }
+    ctx.lineTo(this.xEnd + this.leg, this.y0);
+    ctx.stroke();
+  }
+
+  calc(x) {
+    if (x > this.xStart - this.leg && x < this.xStart) {
+      return this.y0;
+    }
+    if (x > this.xEnd && x < this.xEnd + this.leg) {
+      return this.y0;
+    }
+    if (x >= this.xStart && x <= this.xEnd) {
+      let i = x - this.xStart;
+      let y = this.y0 + this.amp * Math.sin(this.dTh * i);
+      return y;
+    }
+    return;
+  }
+
+  toggle() {
+    this.on = !this.on;
+  }
+}
--- main.js
+++ main.js
@@ -9,32 +9,21 @@ let fps = 60;
 let dt = 1 / fps;
 setInterval(draw, 1000 / fps);
 
+let amp = 50;
+let xStart = 100;
+let xEnd = canvas.width - 100;
+let y0 = canvas.height / 2;
+let wave = 10;
+let leg = 50;
+let inductor = new Inductor(amp, xStart, xEnd, y0, wave, leg);
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.save();
-  //ctx.translate(canvas.height / 2, canvas.height / 2);
-  // Put here the content
+  inductor.draw(ctx);
+}
 
-  ctx.strokeStyle = "black";
-  ctx.lineWidth = 4;
-  ctx.beginPath();
-  let amp = 50;
-  let xStart = 100;
-  let xEnd = canvas.width - 100;
-  let y0 = canvas.height / 2;
-  let y = y0;
-  let th = 0;
-  let wave = 10;
-  let dTh = (wave * (2 * Math.PI)) / (xEnd - xStart);
-  ctx.moveTo(xStart - 50, y);
-  ctx.lineTo(xStart, y);
-  for (let x = xStart; x < xEnd; x += 1) {
-    y = y0 + amp * Math.sin(th);
-    th += dTh;
-    ctx.lineTo(x, y);
+function keypressed(e) {
+  if (e.code === "Space") {
+    inductor.toggle();
   }
-  ctx.lineTo(xEnd + 50, y);
-
-  ctx.stroke();
-  ctx.restore();
 }
