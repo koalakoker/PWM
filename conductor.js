a3625b442050cc9a579a97b3689d391834bec829
Gianluigi Forte <koalakoker@gmail.com>
2024-03-29 06:34:27 +0100

    Conductor working with segments

--- conductor.js
+++ conductor.js
@@ -3,14 +3,19 @@ class Conductor {
     this.a = new Point(100, 200);
     this.b = new Point(200, 50);
     this.off = 0;
-    this.segment = 1;
+    this.segment = 5;
   }
   draw(ctx) {
     // Frame
     drawLine(ctx, this.a, this.b, "black", 4);
     // Electron
-    let ePos = lerpPoint(this.a, this.b, this.off);
-    drawPoint(ctx, ePos, "red", 5);
+    let a = this.a;
+    for (let e = 0; e < this.segment; e++) {
+      let b = lerpPoint(this.a, this.b, (e + 1) / this.segment);
+      let ePos = lerpPoint(a, b, this.off);
+      drawPoint(ctx, ePos, "red", 5);
+      a = b;
+    }
     // Update
     this.update();
   }
