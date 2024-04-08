263cc26f106e0d2b8b0c491c0d1e1a8f9ffec5a5
Gianluigi Forte <koalakoker@gmail.com>
2024-04-08 19:33:39 +0200

    Change dynamically the compare register with the mouse

--- PWM/Output.js
+++ PWM/Output.js
@@ -6,9 +6,14 @@ class Output {
   draw(ctx) {
     let startX = this.PWM.getStartX();
     let bottomLevel = this.PWM.origin.y + this.margin + this.onLevel;
-    startPoly(ctx, new Point(startX, bottomLevel), "red", 3);
+    if (this.PWM.counter.counterValues[0] >= this.PWM.compare.val) {
+      startPoly(ctx, new Point(startX, bottomLevel - this.onLevel), "red", 3);
+    } else {
+      startPoly(ctx, new Point(startX, bottomLevel), "red", 3);
+    }
+
     for (let x = 0; x < this.PWM.length; x++) {
-      let pOn = new Point(startX + x, bottomLevel + this.onLevel);
+      let pOn = new Point(startX + x, bottomLevel - this.onLevel);
       let pOff = new Point(startX + x, bottomLevel);
       if (this.PWM.counter.counterValues[x] >= this.PWM.compare.val) {
         moveTo(ctx, pOn);
