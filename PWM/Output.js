b9805c0455bc6886ead35fd1d75c145744fcfcb3
Gianluigi Forte <koalakoker@gmail.com>
2024-04-09 06:55:43 +0200

    Implementing output mode

--- PWM/Output.js
+++ PWM/Output.js
@@ -1,7 +1,8 @@
 class Output {
-  constructor() {
+  constructor(mode) {
     this.onLevel = 100;
     this.margin = 10;
+    this.mode = mode;
   }
   draw(ctx) {
     let startX = this.PWM.getStartX();
@@ -15,11 +16,21 @@ class Output {
     for (let x = 0; x < this.PWM.length; x++) {
       let pOn = new Point(startX + x, bottomLevel - this.onLevel);
       let pOff = new Point(startX + x, bottomLevel);
+      let state;
       if (this.PWM.counter.counterValues[x] >= this.PWM.compare.val) {
-        moveTo(ctx, pOn);
+        if (this.mode === 0) {
+          state = pOn;
+        } else {
+          state = pOff;
+        }
       } else {
-        moveTo(ctx, pOff);
+        if (this.mode === 0) {
+          state = pOff;
+        } else {
+          state = pOn;
+        }
       }
+      moveTo(ctx, state);
     }
     endPoly(ctx);
   }
